eae36dfdbaddc66b3564ea58fa0056b51164405c
Sergio <prgsergiomb@gmail.com>
2023-02-17 09:25:40 +0100

    1

---
+++ src/programa/Escacs.java
@@ -0,0 +1,73 @@
+package programa;
+
+import Teclat.*;
+
+public class Escacs {
+
+	public static void main(String[] args) {
+
+		char[][] tauler;
+		tauler = Funcions.definirTauler();
+
+		int opcio = 0;
+		while (opcio != 5) {
+
+			Funcions.mostrarTauler(tauler);
+
+			System.out.println();
+			System.out.println("//////////////////////////////////////////");
+			Pantalla.titol("ET MOSTRE EL SEGÜENT MENÚ");
+			System.out.println("1- Posar una peça.");
+			System.out.println("2- Mostrar els moviments d'una peça.");
+			System.out.println("3- Esborrar els moviments.");
+			System.out.println("4- Esborrar el tauler (Peces i Moviments).");
+			System.out.println("5- Eixir.");
+			System.out.println();
+			Pantalla.titolet("SELECCIONA UNA DE LE OPCIONS ANTERIORS: ");
+
+			opcio = Teclat.lligInt("Opció: ", 1, 5);
+			char p;
+			int f;
+			int c;
+			switch (opcio) {
+			case 1:
+				p = Funcions.seleccionarPeça();
+				f = Funcions.demanarCoordenades();
+				c = Funcions.demanarCoordenades();
+				tauler[f][c] = p;
+				break;
+
+			case 2:
+				f = Funcions.demanarCoordenades();
+				c = Funcions.demanarCoordenades();
+				switch (tauler[f][c]) {
+
+				case 'R':
+					Funcions.movimentsRey(tauler, f, c);
+					break;
+					
+				case 'T':
+					Funcions.movimentsTorre(tauler, f, c);
+					break;
+					
+				case 'A':
+					Funcions.movimentsAlfil(tauler, f, c);
+					break;
+				case 'C':
+					Funcions.movimentsCaball(tauler, f, c);
+					break;
+				case 'D':
+					Funcions.movimentsDama(tauler, f, c);
+					break;
+				}
+				
+			case 3:
+				
+
+			}
+
+		}
+		System.out.println("FINISH");
+	}
+
+}
---
+++ src/programa/Funcions.java
@@ -0,0 +1,301 @@
+package programa;
+
+import Teclat.Teclat;
+
+public class Funcions {
+
+	// Variavles globals
+	static final char BUIT = ' ';
+	static int qFiles, qColumnes;
+	static final char MOVIMENTS = 'X';
+
+	// Funció per definir el tauler
+	static char[][] definirTauler() {
+
+		qFiles = Teclat.lligInt("Files que tindrà el tauler: ", 5, 9);
+		qColumnes = Teclat.lligInt("Columnes que tindrà el tauler: ", 5, 9);
+
+		char[][] tauler = new char[qFiles + 1][qColumnes + 1];
+
+		for (int f = 1; f <= qFiles; f++) {
+			for (int c = 1; c <= qColumnes; c++) {
+				tauler[f][c] = BUIT;
+			}
+		}
+		return tauler;
+	}
+
+	// Procediment per mostrar el tauler
+	static void mostrarTauler(char[][] tauler) {
+
+		System.out.println(" ");
+		for (int c = 1; c < tauler.length; c++) {
+			System.out.print(" " + c);
+		}
+
+		System.out.println();
+		for (int f = 1; f < tauler.length; f++) {
+			System.out.print(f);
+			for (int c = 1; c < tauler.length; c++) {
+				System.out.print(" " + tauler[f][c]);
+			}
+			System.out.println();
+
+		}
+
+	}
+
+	// Funció seleccionar peça
+	static char seleccionarPeça() {
+
+		char peça;
+		return peça = Teclat.lligChar("Quina peça vols posar. Rey,Torre,Dama,Caball,Alfil: ", "rtdca");
+	}
+
+	// Funció demanar Fila
+	static int demanarCoordenades() {
+
+		int fc;
+		return fc = Teclat.lligInt("Nº de Fila i Columna:", 5, 9);
+
+	}
+
+	// Procediment moviments peces
+
+	static void movimentsRey(char[][] t, int f, int c) {
+
+		if (f - 1 >= 1 && c - 1 >= 1) {
+			if (t[f - 1][c - 1] == BUIT) {
+				t[f - 1][c - 1] = MOVIMENTS;
+			}
+
+		}
+		if (f + 1 <= qFiles && c + 1 <= qColumnes) {
+			if (t[f + 1][c + 1] == BUIT) {
+				t[f + 1][c + 1] = MOVIMENTS;
+			}
+
+		}
+		if (f - 1 >= 1 && c + 1 <= qColumnes) {
+			if (t[f - 1][c + 1] == BUIT) {
+				t[f - 1][c + 1] = MOVIMENTS;
+			}
+
+		}
+		if (f + 1 <= qFiles && c - 1 >= 1) {
+			if (t[f + 1][c - 1] == BUIT) {
+				t[f + 1][c - 1] = MOVIMENTS;
+			}
+
+		}
+		if (c - 1 >= 1) {
+			if (t[f][c - 1] == BUIT) {
+				t[f][c - 1] = MOVIMENTS;
+			}
+
+		}
+		if (c + 1 <= qColumnes) {
+			if (t[f][c + 1] == BUIT) {
+				t[f][c + 1] = MOVIMENTS;
+			}
+
+		}
+		if (f - 1 >= 1) {
+			if (t[f - 1][c] == BUIT) {
+				t[f - 1][c] = MOVIMENTS;
+			}
+
+		}
+		if (f + 1 <= qFiles) {
+			if (t[f + 1][c] == BUIT) {
+				t[f + 1][c] = MOVIMENTS;
+			}
+
+		}
+
+	}
+
+	static void movimentsTorre(char[][] t, int f, int c) {
+
+		for (int c1 = c - 1; c1 >= 1; c1--) {
+			if (t[f][c1] != BUIT && t[f][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f][c1] = MOVIMENTS;
+
+		}
+		for (int c1 = c + 1; c1 <= qColumnes; c1++) {
+			if (t[f][c1] != BUIT && t[f][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f][c1] = MOVIMENTS;
+
+		}
+		for (int f1 = f - 1; f1 >= 1; f1--) {
+			if (t[f1][c] != BUIT && t[f1][c] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c] = MOVIMENTS;
+
+		}
+		for (int f1 = f + 1; f1 <= qFiles; f1++) {
+			if (t[f1][c] != BUIT && t[f1][c] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c] = MOVIMENTS;
+		}
+
+	}
+
+	static void movimentsAlfil(char[][] t, int f, int c) {
+
+		for (int f1 = f - 1, c1 = c - 1; f1 >= 1 && c1 >= 1; f1--, c1--) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+		}
+		for (int f1 = f + 1, c1 = c + 1; f1 <= qFiles && c1 <= qColumnes; f1++, c1++) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+		}
+		for (int f1 = f - 1, c1 = c + 1; f1 >= 1 && c1 <= qFiles; f1--, c1++) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+		}
+		for (int f1 = f + 1, c1 = c - 1; f1 <= qFiles && c1 >= 1; f1++, c1--) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+
+		}
+	}
+
+	static void movimentsCaball(char[][] t, int f, int c) {
+
+		try {
+			if (t[f + 1][c - 2] == BUIT) {
+				t[f + 1][c - 2] = MOVIMENTS;
+			}
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f + 2][c - 1] == BUIT) {
+				t[f + 2][c - 1] = MOVIMENTS;
+			}
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f - 1][c - 2] == BUIT) {
+				t[f - 1][c - 2] = MOVIMENTS;
+			}
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f - 2][c - 1] == BUIT)
+				t[f - 2][c - 1] = MOVIMENTS;
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f - 2][c + 1] == BUIT)
+				t[f - 2][c + 1] = MOVIMENTS;
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f - 1][c + 2] == BUIT)
+				t[f - 1][c + 2] = MOVIMENTS;
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f + 1][c + 2] == BUIT)
+				t[f + 1][c + 2] = MOVIMENTS;
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+		try {
+			if (t[f + 2][c + 1] == BUIT)
+				t[f + 2][c + 1] = MOVIMENTS;
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+		}
+
+	}
+
+	static void movimentsDama(char[][] t, int f, int c) {
+
+		for (int f1 = f - 1, c1 = c - 1; f1 >= 1 && c1 >= 1; f1--, c1--) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+		}
+		for (int f1 = f + 1, c1 = c + 1; f1 <= qFiles && c1 <= qColumnes; f1++, c1++) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+		}
+		for (int f1 = f - 1, c1 = c + 1; f1 >= 1 && c1 <= qFiles; f1--, c1++) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+		}
+		for (int f1 = f + 1, c1 = c - 1; f1 <= qFiles && c1 >= 1; f1++, c1--) {
+			if (t[f1][c1] != BUIT && t[f1][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c1] = MOVIMENTS;
+
+		}
+
+		for (int c1 = c - 1; c1 >= 1; c1--) {
+			if (t[f][c1] != BUIT && t[f][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f][c1] = MOVIMENTS;
+
+		}
+		for (int c1 = c + 1; c1 <= qColumnes; c1++) {
+			if (t[f][c1] != BUIT && t[f][c1] != MOVIMENTS) {
+				break;
+			}
+			t[f][c1] = MOVIMENTS;
+
+		}
+		for (int f1 = f - 1; f1 >= 1; f1--) {
+			if (t[f1][c] != BUIT && t[f1][c] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c] = MOVIMENTS;
+
+		}
+		for (int f1 = f + 1; f1 <= qFiles; f1++) {
+			if (t[f1][c] != BUIT && t[f1][c] != MOVIMENTS) {
+				break;
+			}
+			t[f1][c] = MOVIMENTS;
+		}
+
+	}
+
+}
